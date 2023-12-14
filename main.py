9f0f588f4df7a0eec08318966ede77de373a7641
shivakumarsanugula <shivakumaryadav1997@gmail.com>
2023-12-14 13:33:22 +0530

    Added Model_Trainer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CnnClassifier import logger
 from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CnnClassifier.pipeline.stage_03_modeltrainer import ModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 try:
@@ -17,6 +18,18 @@ STAGE_NAME = 'Prepare base model'
 try:
     logger.info(f">>>>>>>> Stage {STAGE_NAME} started >>>>>>>>>>>>>>")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>stage {STAGE_NAME} complted <<<<<<<<<<<<<\n\nx================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Training'
+
+try:
+    logger.info(f">>>>>>>> Stage {STAGE_NAME} started >>>>>>>>>>>>>>")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>stage {STAGE_NAME} complted <<<<<<<<<<<<<\n\nx================x")
 except Exception as e:
---
+++ src/CnnClassifier/pipeline/stage_03_modeltrainer.py
@@ -0,0 +1,28 @@
+from CnnClassifier.config.configuration import ConfigurationManager
+from CnnClassifier.components.model_trainer import Training
+from CnnClassifier import logger
+
+
+STAGE_NAME = 'Training'
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> Stage {STAGE_NAME} started >>>>>>>>>>>>>>")
+        obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>stage {STAGE_NAME} complted <<<<<<<<<<<<<\n\nx================x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
